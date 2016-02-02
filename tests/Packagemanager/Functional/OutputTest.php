a6000bea91a94580982b10a009ae0b529ad0c8e5
Nicole Cordes <cordes@cps-it.de>
2016-02-02 18:50:05 +0100

    [TASK] Add further composer output tests

--- tests/Packagemanager/Functional/OutputTest.php
+++ tests/Packagemanager/Functional/OutputTest.php
@@ -23,4 +23,56 @@ class OutputTest extends ComposerTestCase
         $this->assertContains('Using version ^1.0 for ichhabrecht/packagemanager', $bufferedOutput->fetch());
     }
 
+    public function testComposerOutputForInstallIsIntact()
+    {
+        $composerFile = self::createComposerJson();
+        $fileHandle = fopen($composerFile, 'wb');
+        fwrite($fileHandle, json_encode([
+            'require' => [
+                'ichhabrecht/packagemanager' => '1.0',
+            ],
+        ]));
+        fclose($fileHandle);
+
+        $bufferedOutput = new BufferedOutput();
+
+        self::callComposer(
+            'install',
+            false,
+            [
+                '--dry-run',
+            ],
+            null,
+            $bufferedOutput
+        );
+
+        $this->assertContains('  - Installing ichhabrecht/packagemanager (1.0)', $bufferedOutput->fetch());
+    }
+
+    public function testComposerOutputForUpdateIsIntact()
+    {
+        $composerFile = self::createComposerJson();
+        $fileHandle = fopen($composerFile, 'wb');
+        fwrite($fileHandle, json_encode([
+            'require' => [
+                'ichhabrecht/packagemanager' => '1.0',
+            ],
+        ]));
+        fclose($fileHandle);
+
+        $bufferedOutput = new BufferedOutput();
+
+        self::callComposer(
+            'update',
+            false,
+            [
+                '--dry-run',
+            ],
+            null,
+            $bufferedOutput
+        );
+
+        $this->assertContains('  - Installing ichhabrecht/packagemanager (1.0)', $bufferedOutput->fetch());
+    }
+
 }
